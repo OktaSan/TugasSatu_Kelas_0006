a7b575b7f209c930efeefc6370b4e5b538ace44d
Okta Putra Ramadhan <oktaxd88@gmail.com>
2026-05-08 10:29:28 +0700

    rebuild dan juga debug event button

--- script.js
+++ script.js
@@ -1,91 +1,13 @@
-const inputValue = document.getElementById("inputTask");
-const btnTambahTodo = document.getElementById("btnTambahTodo");
+const formTugas = document.getElementById("todoForm");
+const inputTugas = document.getElementById("inputTask");
 const inputTanggal = document.getElementById("tanggal");
+const tombolSubmit = document.getElementById("btnTambahTodo");
 const daftarTugas = document.getElementById("listTugas");
 
-btnTambahTodo.addEventListener("click",function(){
+let dataTugas = [];
+let idTugasYangDiEdit = null;
 
-    if(inputValue.value === ""){
-        alert("input tidak boleh kosong!");
-        return;
-    }
-
-    const listBaru = document.createElement("li");
-    const span = document.createElement("span");
-    span.innerHTML = inputValue.value;
-
-    const tanggal = inputTanggal.value;
-    if (tanggal === "") {
-        alert("Tanggal harus diisi!");
-        return;
-    }
-    const item = document.createElement("span");
-    item.classList.add("deadline");
-    item.textContent = " deadline: " + tanggal;
-
-    const btnEdit = document.createElement("button");
-    btnEdit.textContent = "Edit";
-
-    const btnHapus = document.createElement("button");
-    btnHapus.textContent = "Hapus";
-
-    btnEdit.addEventListener("click",function(){
-        inputValue.value = span.textContent;
-
-        daftarTugas.removeChild(listBaru);
-    });
-
-    btnHapus.addEventListener("click",function(){
-
-        daftarTugas.removeChild(listBaru);
-    });
-
-
-    const status = document.createElement("status");
-    status.textContent = " Progress ";
-
-    const btnStatus = document.createElement("button");
-    btnStatus.textContent = "Selesai";
-
-    btnStatus.addEventListener("click", function () {
-
-        if (status.textContent === " Progress ") {
-
-            status.textContent = " Done ";
-            btnStatus.textContent = "Progress";
-
-        } else {
-
-            status.textContent = " Progress ";
-            btnStatus.textContent = "Selesai";
-
-        }
-
-    });
-
-    listBaru.appendChild(span);
-    listBaru.appendChild(item);
-    listBaru.appendChild(status);
-    listBaru.appendChild(btnStatus);
-    listBaru.appendChild(btnEdit);
-    listBaru.appendChild(btnHapus);
-
-    daftarTugas.appendChild(listBaru);
-
-    inputValue.value= "";
-    inputTanggal.value="";
-    inputValue.focus();
-
-    
-
-});
-
-
-
-
-
-
-
-
-
-
+formTugas.addEventListener("submit", function (event) {
+    event.preventDefault();
+    console.log("Button berhasil di tekan!")
+})
